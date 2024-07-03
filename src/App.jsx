e7c04773d0063e4e978864f90b2969686e6847d5
Rajesh <rajeshkmrmanda@gmail.com>
2024-07-03 16:20:42 -0500

    add button disable feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
+import Log from "./components/Log";
+import { useState } from "react";
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+    currentPlayer = 'O'
+  }
+
+  return currentPlayer
+}
 
 function App() {
+  const [gameTurns,setGameTurns] = useState([]);
+  // const [activePlayer,setActivePlayer] = useState('X');
+
+  const activePlayer = deriveActivePlayer(gameTurns)
+
+  function handleSelectSquare(rowIndex,colIndex){
+    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X' )
+    setGameTurns(prevTurns => {
+      const currentPlayer = deriveActivePlayer(prevTurns)
+      
+      const updatedTurns = [
+        {square: {row: rowIndex, col: colIndex}, 
+        player: currentPlayer},...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  }
+
   return <main>
     <div id="game-container">
-      <ol id="players">
-        <Player initialName='player 1' symbol='X'/>
-        <Player initialName='player 2' symbol='O'/>
+      <ol id="players" className="highlight-player">
+        <Player initialName='player 1' symbol='X' isActive={activePlayer === 'X'}/>
+        <Player initialName='player 2' symbol='O' isActive={activePlayer === 'O'}/>
       </ol>
-      GAME BOARD
+      <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
     </div>
+    <Log turns={gameTurns} />
   </main>
 }
 
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -31,7 +31,9 @@ export default function GameBoard({onSelectSquare,turns}){
             <ol>
                 {row.map((playerSymbol,colIndex) =>
                     <li key= {colIndex}> 
-                    <button onClick={() => onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button></li>)}
+                    <button onClick={() => onSelectSquare(rowIndex,colIndex)} disabled={(playerSymbol !== null)} >
+                        {playerSymbol}
+                    </button></li>)}
             </ol>
         </li>)}
     </ol>
